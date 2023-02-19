2977a95d76cec8112632ee44dd4c4b8f4caaad8e
tweaky <gleb2604@bk.ru>
2023-02-19 19:31:18 +0300

    сделал первый этап задачи

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] array = new string[]   {"Twenty five again",
+                                "Twe", "nty", "aga", "in", "=", "45"};
+
+string[] newArray = new string[SearchCount()];
+
+int SearchCount()
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+    if (array[i].Length <= 3)
+        {
+            count+=1;
+        }
+    }
+    return count;
+}
